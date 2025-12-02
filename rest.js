0b10089988f08524e230d7a04fd5e98f96d090ca
Suhana <suhanabansal4@gmail.com>
2025-12-02 15:26:29 +0530

    .js

---
+++ rest.js
@@ -0,0 +1,3 @@
+function test(firstargument,...otherArguments){
+  
+}
---
+++ var_let_const.js
@@ -0,0 +1,9 @@
+function main(){
+  var x = 10;
+  x = 20; //reassign 
+  if (true){
+  var x = 30;
+  }
+  console.log(x);
+}
+main();
